59bcda0a6234e3a8e75843a1fb4a2ce1e901d00f
Júlio Moura <julio.moura@aisdigital.com.br>
2019-11-16 02:05:28 -0300

    First commit

---
+++ src/main/java/com/julio/oauth2google/Oauth2googleApplication.java
@@ -0,0 +1,13 @@
+package com.julio.oauth2google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2googleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2googleApplication.class, args);
+	}
+
+}
