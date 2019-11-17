7c596010e126e8cf0d97642f1822d06ab090ca5a
Júlio Moura <julio.moura@aisdigital.com.br>
2019-11-17 00:38:13 -0300

    Insert endpoint to user Principal

--- src/main/java/com/julio/oauth2google/Oauth2googleApplication.java
+++ src/main/java/com/julio/oauth2google/Oauth2googleApplication.java
@@ -2,12 +2,22 @@ package com.julio.oauth2google;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@RestController
 public class Oauth2googleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2googleApplication.class, args);
+	}
+
+	@RequestMapping(value = "/user")
+	public Principal user(Principal principal) {
+		return principal;
 	}
 
 }
