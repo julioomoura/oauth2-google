d871cb6287fdf5afe5f9073501e50e41f6f517e9
Júlio Moura <julio.moura@aisdigital.com.br>
2019-11-17 00:38:13 -0300

    Insert endpoint to user Principal

--- src/main/java/com/julio/oauth2google/config/WebSecurityConfiguration.java
+++ src/main/java/com/julio/oauth2google/config/WebSecurityConfiguration.java
@@ -9,15 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableOAuth2Sso
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf()
+                .csrf()
                 .disable()
                 .antMatcher("/**")
                 .authorizeRequests()
                 .antMatchers("/", "/index.html")
-                .permitAll()
-                .antMatchers()
                 .permitAll()
                 .anyRequest()
                 .authenticated();
